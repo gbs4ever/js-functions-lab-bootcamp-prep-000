6f5841525c91e69a482044894e79ab0c300c1472
s greenbaum <gbs4ever2@gmail.com>
2018-11-18 20:37:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ function  happyHolidays() {return('Happy holidays!')
 
 }
 function happyHolidaysTo(name){return(`Happy holidays, ${name}!`)
+
+}
+function happyHolidaysTo(string) {return(`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown() {return(`It's ${days} days until ${holiday}!`)
+  
 }
